f1e7ea7a546a5ef2e94cf4cf95a72f8863d8f6f9
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-10-10 01:46:33 +0100

    updated files with isValid

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ document.addEventListener('DOMContentLoaded', function(){
         const Email=document.getElementById('email').value.trim();
         const password=document.getElementById('password').value.trim();
 
-        let isvalid=true;
+        let isValid=true;
         let messages=[];
         if(username.length < 3){
-           return isvalid=false;
+           return isValid=false;
             messages=['username too short'];
                }
 
                if(!Email.includes('@') && Email.includes('.')){
-                return isvalid=false;
+                return isValid=false;
                 messages.push("Email must contain '@' and '.'");
 
                }
                if(password.lenght >= 8){
-                return isvalid=true;
+                return isValid=true;
                }else{
-                isvalid=false;
+                isValid=false;
                 messages=('password too short, password must be greater than 8 characters');
                }
            feedbackDiv.style.display="block";
-           if(isvalid=true){
+           if(isValid=true){
             feedbackDiv.innerHTML="Registration successful!";
             feedbackDiv.style.color="#28a745";
            } else{
