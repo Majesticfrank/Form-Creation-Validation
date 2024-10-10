694d3e1b0865bdaa2bc9d1c62401adee35b8ad4d
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-10-11 00:07:54 +0100

    updated files

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
                 messages.push("Email must contain '@' and '.'");
 
                }
-               if(password.length>= 8){
+               if(password.length>=8){
                 return isValid=true;
                }else{
                 isValid=false;
